18fa315e3ae9ba4f83539115accbb1db3cfcd13f
Ashwani Ahlawat <ashuahlawat22@gmail.com>
2020-12-11 04:26:22 +0530

    progress

--- NumpyModule/1array.py
+++ NumpyModule/1array.py
@@ -2,7 +2,7 @@
 import numpy
 
 arrp=[[1,2,3,4,5], [6,7,8,9,10]]
-arr = numpy.array([[1,2,3,4,5], [6,7,8,9,10]])
+arr = numpy.array(arrp)
 
 print('Last element from 2nd dim: ', arr[1, -1])
 print(arrp[1][-1])
