ffceb7409dffb7015d8ddaa0abd90d6b9997d525
Ashwani Ahlawat <ashuahlawat22@gmail.com>
2021-08-25 04:43:12 +0530

    name changed

--- WebScrapping/5novelDownload.py
+++ WebScrapping/5novelDownload.py
@@ -3,8 +3,8 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-driver = webdriver.Chrome(executable_path="C:\ProgramData\chromedriver.exe")
-driver.get("https://www.novels.pl/novel/I-Shall-Seal-the-Heavens/201/Chapter-201-The-Dao-Child-Fights.html")
+driver = webdriver.Chrome(executable_path="C:\ProgramData\chromedriver92.exe")
+driver.get("https://www.novels.pl/novel/I-am-the-Monarch/221/Chapter-220--Amaranth-20.html")
 
 import time
 
@@ -12,18 +12,29 @@ def downloadloop(x):
     try:
         match = WebDriverWait(driver,3).until(
             EC.presence_of_element_located(
-            (By.ID,'aswift_7')
+            (By.ID,'aswift_9')
             )
         )
-        iframe = driver.find_element_by_id('aswift_7')
+        iframe = driver.find_element_by_id('aswift_9')
         driver.switch_to.frame(iframe)
         search_a = driver.find_element_by_id("dismiss-button")
         search_a.click()
-    except Exception as e:
-        print(e)
+    except:
+        try:
+            match = WebDriverWait(driver,3).until(
+                EC.presence_of_element_located(
+                (By.ID,'ad_iframe')
+                )
+            )
+            iframe = driver.find_element_by_id('ad_iframe')
+            driver.switch_to.frame(iframe)
+            search_a = driver.find_element_by_id("dismiss-button")
+            search_a.click()
+        except Exception as e:
+            print(e)
     finally:
         driver.switch_to.default_content()
-    driver.switch_to.default_content()
+    
     match = WebDriverWait(driver,10).until(
         EC.presence_of_element_located(
             (By.CLASS_NAME,'_download')
@@ -40,4 +51,4 @@ def downloadloop(x):
     x = x+1
     downloadloop(x)
 
-downloadloop(201)
+downloadloop(220)
