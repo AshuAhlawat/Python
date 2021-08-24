248e3b0cf2897da6f0b772b2e041306ba6cee859
Ashwani Ahlawat <ashuahlawat22@gmail.com>
2021-08-25 04:43:59 +0530

    diamond pattern

---
+++ Practice/diamond.py
@@ -0,0 +1,19 @@
+from _timer import timeit
+
+
+
+def main():
+    x = 5
+    
+    for i in range(x+1):
+        for j in range(i):
+            print("*",end="")
+        print("")
+
+    for i in range(x+1):
+        for j in range(x+1-i):
+            print("*",end="")
+        print("")
+
+
+timeit(main)
